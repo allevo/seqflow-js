899006316a9b88d95633ad91b6f805cc30ba2c81
Tommaso Allevi <tomallevi@gmail.com>
2024-07-19 19:20:34 +0200

    Add test component

---
+++ packages/components/tests/test.js
@@ -0,0 +1,13 @@
+import { spawn, spawnSync } from 'node:child_process';
+import waitOn from 'wait-on';
+
+const storybookProcess = spawn('pnpm', ['run', 'storybook'], { stdio: 'inherit' })
+
+await waitOn({
+    resources: ['http://localhost:6006'],
+    timeout: 10_000
+})
+
+spawnSync('pnpm', ['test:storybook'], { stdio: 'inherit' })
+
+storybookProcess.kill('SIGINT')
