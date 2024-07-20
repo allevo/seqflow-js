d5f09c205a1e0e84e631fb39785ef1161bc41fe6
Tommaso Allevi <tomallevi@gmail.com>
2024-07-20 16:54:58 +0200

    Re-enable them

--- packages/components/tests/test.js
+++ packages/components/tests/test.js
@@ -1,17 +1,34 @@
-/*
+
 import { spawn, spawnSync } from 'node:child_process';
 import waitOn from 'wait-on';
 
-const storybookProcess = spawn('pnpm', ['run', 'storybook'], { stdio: 'inherit' })
+console.log('STARTING STORYBOOK')
 
-await waitOn({
-    resources: ['http://localhost:6006'],
-    timeout: 10_000
+const storybookProcess = spawn('pnpm', ['run', 'storybook'], { stdio: 'inherit' })
+storybookProcess.on('exit', (code) => {
+    console.log(`--- Storybook exited with code ${code}`)
 })
 
-spawnSync('pnpm', ['test:storybook'], { stdio: 'inherit' })
+console.log('WAITING FOR STORYBOOK TO START')
+try {
+    await waitOn({
+        resources: ['http://localhost:6006'],
+        timeout: 10_000
+    })
+} catch (error) {
+    console.error('Storybook did not start in time', error)
+    process.exit(1)
+}
+
+console.log('STARTING STORYBOOK TESTS')
+try {
+    spawnSync('pnpm', ['test:storybook'], { stdio: 'inherit' })
+} catch (error) {
+    console.error('Storybook tests failed', error)
+    process.exit(1)
+}
+console.log('STORYBOOK TESTS PASSED')
 
 storybookProcess.kill('SIGINT')
-*/
 
 console.log('SKIPPED')
