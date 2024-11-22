891db6a23a42bcf80ebd555e98151064351851a9
Tommaso Allevi <tomallevi@gmail.com>
2024-11-22 17:25:50 +0100

    Fix lint

--- packages/website/src/pages/blog/Blog.tsx
+++ packages/website/src/pages/blog/Blog.tsx
@@ -1,9 +1,8 @@
 import { Divider, Heading, Hero, Link, Prose, Tabs } from "@seqflow/components";
 import { ComponentProps, Contexts } from "@seqflow/seqflow";
-import classes from './Blog.module.css';
+import classes from "./Blog.module.css";
 
 import { html } from "./2024-11-24-theres-a-new-framework-in-town.md";
-
 
 export async function Blog(
 	_: ComponentProps<unknown>,
@@ -12,12 +11,12 @@ export async function Blog(
 	component._el.style.backgroundColor = "rgb(6, 6, 6)";
 	component._el.style.minHeight = "100vh";
 
-	component._el.style.paddingTop = '15px'
+	component._el.style.paddingTop = "15px";
 
 	const segments = app.router.segments;
 	if (segments.length === 1) {
 		component.renderSync(
-			<Prose className={['m-auto']} style={{ maxWidth: '786px'}}>
+			<Prose className={["m-auto"]} style={{ maxWidth: "786px" }}>
 				<Heading level={1}>Blog</Heading>
 				<ol>
 					<li>
@@ -27,7 +26,9 @@ export async function Blog(
 							</Link>
 						</Heading>
 						<p>
-							Release 1.0.0-rc1 is out! We are excited to announce the first release candidate of SeqFlow. This release includes a new framework that makes it easier to build web applications.
+							Release 1.0.0-rc1 is out! We are excited to announce the first
+							release candidate of SeqFlow. This release includes a new
+							framework that makes it easier to build web applications.
 						</p>
 					</li>
 				</ol>
@@ -36,27 +37,29 @@ export async function Blog(
 		return;
 	}
 
-    component._el.style.paddingBottom = '15px'
+	component._el.style.paddingBottom = "15px";
 
 	const segment = segments[1];
 	switch (segment) {
-		case '2024-11-24-theres-a-new-framework-in-town':
+		case "2024-11-24-theres-a-new-framework-in-town": {
 			const element = getElementFromString(html);
 
 			component.renderSync([
-				<Prose className={['m-auto', classes.blogPost]}>
-					<Heading className={'text-center'} level={1}>There's a New Framework in Town!</Heading>
+				<Prose className={["m-auto", classes.blogPost]}>
+					<Heading className={"text-center"} level={1}>
+						There's a New Framework in Town!
+					</Heading>
 					<img
 						src="/images/blog/2024-11-24-theres-a-new-framework-in-town.svg"
-						alt="A beautiful image that show the SeqFlow logo"
-						style={{ borderRadius: '15px', border: '1px solid #2b2f35' }}
+						alt="the amazing SeqFlow logo"
+						style={{ borderRadius: "15px", border: "1px solid #2b2f35" }}
 					/>
 					{element}
 				</Prose>,
 			]);
 			break;
+		}
 	}
-
 }
 
 function getElementFromString(string: string) {
